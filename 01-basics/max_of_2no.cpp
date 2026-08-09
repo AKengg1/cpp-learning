a042c6ffef0f5d619c2d36f1d193caf53c4fa133
AKengg1 <ak72532961@gmail.com>
2026-08-09 07:43:31 +0530

    function assignment

---
+++ 01-basics/max_of_2no.cpp
@@ -0,0 +1,19 @@
+// 9- Maximum of Two Numbers
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    if (a > b)
+    {
+        cout << "Greater number is " << a << endl;
+    }
+    else
+    {
+        cout << "Greater number is " << b << endl;
+    }
+
+    return 0;
+}
