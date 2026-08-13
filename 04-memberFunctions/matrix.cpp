16f7be7e6ad9b9a20af0bf55c767ed96c6730fd8
AKengg1 <ak72532961@gmail.com>
2026-08-14 02:14:26 +0530

    member functions assignment complete

--- 04-memberFunctions/matrix.cpp
+++ 04-memberFunctions/matrix.cpp
@@ -1,3 +1,85 @@
+// 4-matrix and class
+#include <iostream>
+using namespace std;
+class Matrix
+{
+private:
+    int A[3][3];
+
+public:
+    void input()
+    {
+        cout << "Enter 9 matrix elements row wise";
+        for (int i = 0; i <= 2; i++)
+            for (int j = 0; j <= 2; j++)
+                cin >> A[i][j];
+    }
+    void display()
+    {
+        for (int i = 0; i <= 2; i++)
+        {
+            for (int j = 0; j <= 2; j++)
+                cout << A[i][j] << "  ";
+            cout << endl;
+        }
+    }
+    Matrix add(Matrix M)
+    {
+        Matrix temp;
+        for (int i = 0; i <= 2; i++)
+        {
+            for (int j = 0; j <= 2; j++)
+                temp.A[i][j] = A[i][j] + M.A[i][j];
+        }
+        return temp;
+    }
+    Matrix sub(Matrix M)
+    {
+        Matrix temp;
+        for (int i = 0; i <= 2; i++)
+        {
+            for (int j = 0; j <= 2; j++)
+                temp.A[i][j] = A[i][j] - M.A[i][j];
+        }
+        return temp;
+    }
+    Matrix multiply(Matrix M)
+    {
+        Matrix temp;
+        int sum;
+        for (int i = 0; i <= 2; i++)
+        {
+            for (int j = 0; j <= 2; j++)
+            {
+                for (int k = 0, sum = 0; k <= 2; k++)
+                    sum += A[i][k] * M.A[k][j];
+                temp.A[i][j] = sum;
+            }
+        }
+        return temp;
+    }
+    Matrix transpose()
+    {
+        Matrix temp;
+        for (int i = 0; i <= 2; i++)
+        {
+            for (int j = 0; j <= 2; j++)
+            {
+                temp.A[j][i] = A[i][j];
+            }
+        }
+        return temp;
+    }
+    bool is_singular()
+    {
+        int s;
+        s = A[0][0] * (A[1][1] * A[2][2] - A[2][1] * A[1][2]) - A[0][1] * (A[1][0] * A[2][2] - A[2][0] * A[1][2]) + A[0][2] * (A[1][0] * A[2][1] - A[2][0] * A[1][1]);
+        if (s == 0)
+            return true;
+        else
+            return false;
+    }
+};
 int main()
 {
     Matrix A, B, C;
